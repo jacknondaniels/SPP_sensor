96fafe4746c03e703405626fb06035cb884f447c
jacknondaniels <48773831+jacknondaniels@users.noreply.github.com>
2021-08-19 12:34:14 +0300

    added the dispersion
    refractive index taken from reference data and integrated into calculation of dispersion

--- map_to_data.py
+++ map_to_data.py
@@ -5,7 +5,6 @@ from math import fabs
 
 e_0 = 8.85/10**12
 c = 299792458
-
 e_1 = 1.51**2
 e_2 = (0.18344 + 3.4332j)**2 #au for 633 nm
 e_out = 1
@@ -14,11 +13,35 @@ h = 40e-9
 w_1 = 2690930*10**9 #lambda = 700 nm
 w_2 = 2975753*10**9 #lambda = 633 nm
 
-def get(i, omega, e_3, h1):
+def readf(f):
+    disp = []
+    for line in f:   
+        disp.append([float(line.split()[0]), float(line.split()[1]), float(line.split()[2])])
+    return disp
+
+def dispersion (f, l):
+  e_2 = 1 + 1j
+  Real = []
+  Imaginary = []
+  Length = []
+  for i in range (len(f)):
+    if (fabs(l - f[i][0]*1000) < 100):
+      Length.append(f[i][0]*1000)
+      Real.append(f[i][1])
+      Imaginary.append(f[i][2])
+  S_real = np.polyfit (Length, Real, 1)
+  rl = l*S_real[0] + S_real[1]
+  S_imag = np.polyfit (Length, Imaginary, 1)
+  img = l*S_imag[0] + S_imag[1]
+  return (rl + img*1j)
+
+def get(i, l, e_3, h1, f):
+  omega = 2*3.14*c*10**9/l
   k_x = cm.sqrt(e_1 * omega**2 / (c**2)) * cm.sin(i)
   k1 = omega/c * cm.sqrt(e_1 - e_1* cm.sin(i))
   Z1 = cm.sqrt(e_1 - e_1* cm.sin(i))/e_1
   glass = np.array([[cm.cos(k1*h), -1j* cm.sin(k1*h)/Z1], [-1j* cm.sin(k1*h)*Z1, cm.cos(k1*h)]])
+  e_2 = dispersion (f, l)
   k2 = omega/c * cm.sqrt(e_2 - e_1* cm.sin(i))
   Z2 = cm.sqrt(e_2 - e_1* cm.sin(i))/e_2
   metal = np.array([[cm.cos(k2*h), -1j* cm.sin(k2*h)/Z2], [-1j* cm.sin(k2*h)*Z2, cm.cos(k2*h)]])
@@ -38,57 +61,45 @@ def get(i, omega, e_3, h1):
   return (R)
 
 
-def draw(omega, e_3, h1):
+def draw(l, e_3, h1, disp):
   theta = []
   transmission = []
   resonanse_angle = []
   resonanse = []
   rad = 0
-  for i in range(1000):
+  for i in range(1000): # you need to rewrite algorith of finding the plasmonic resonanse minimum
     rad = rad + 0.00157
     if (rad > 0.55) & (rad < 0.90):
       resonanse_angle.append(rad)
-      resonanse.append(get(rad, omega, e_3, h1))
+      resonanse.append(get(rad, l, e_3, h1, disp))
   min_value = min(resonanse)
   min_index = resonanse.index(min_value)
   #print (resonanse_angle[min_index], omega, h1, e_3)
   return resonanse_angle[min_index]
 
-def find_sense (l, h1):
-  w = 2*3.14*c*10**9/l
+def find_sense (l, h1, disp):
   resonanse = []
   refractive = []
   n = 1.33 #probe
   for i in range(10):
-    resonanse.append(draw (w, n**2, h1))
+    resonanse.append(draw (l, n**2, h1, disp))
     refractive.append(n)
     n = n + 0.01
   S =  np.polyfit(refractive, resonanse, 1)
 #  print(fabs(S[0])*180/3.14, l, h1)
   return (fabs(S[0])*180/3.14)
 
-def draw_sensetivity(h1):
-  sensetivity = []
-  thickness = []
-  for i in range(10):
-    sensetivity.append(find_sense(h1))
-    thickness.append(h1)
-    h1 = h1 + 2e-7
-  fig, ax = plt.subplots(1, 1, figsize=(20, 8))
-  ax.scatter(thickness, sensetivity)
-  ax.set_ylabel ("sensetivity, deg/RIU")
-  ax.set_xlabel ("thickness, m")
-  ax.set_xlim(9e-7, 35e-7)
-  fig.savefig("thickness of probe layer - sensetivity.png")
-  plt.close()
-
-f = open ('data2.txt', 'w')
+f = open ('data.txt', 'w')
+g = open ('disp.txt', 'r')
+au = readf(g)
+g.close()
 lda = np.arange (600, 800, 10)
 thick = np.arange(40e-9, 60e-9, 1e-9)
 z = np.zeros((len(lda), len(thick)))
 for i in range (len(lda)):
   for j in range(len(thick)):
-    z[i][j] = find_sense(lda[i], thick[j])
+    z[i][j] = find_sense(lda[i], thick[j], au)
     f.write (str(z[i][j]) + ' ')
   f.write ('\n')
+  print ("was here")
 f.close()
